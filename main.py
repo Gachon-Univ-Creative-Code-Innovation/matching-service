b971fdf4ff0390c73b0f7eacff7fcec4ef284d73
hssKang <150224614+hssKang@users.noreply.github.com>
2025-05-14 14:40:34 +0900

    test: Pipeline 구성 test
    JIRA: GUC-157
    resolves: #13

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 import asyncio
+import logging
 from fastapi import FastAPI
-from src.Consumer.FastApiConsumer import KafkaTagConsumer
 
+from src.Consumer.FastApiConsumer import KafkaTagConsumer
 from src.Service.SearchUser import SearchUsers
 from src.Consumer.QdrantClient import InitQdrant
 from src.Service.RepresentUser import RepresentTags
 
 app = FastAPI(title="Matching Service")
-consumer = KafkaTagConsumer(["GithubTags", "BlogTags"])
+# consumer = KafkaTagConsumer(["GithubTags", "BlogTags"])
+consumer = KafkaTagConsumer(topics=["server.public.Career_Tag"])
+
+# 로깅 설정
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 # Kafka Consumer 실행
 @app.on_event("startup")
-async def startup_event():
+async def StartupEvent():
+    await consumer.WaitKafka()
+    logger.info("Starting Kafka consumer...")
     await InitQdrant()
-    await consumer.start()
+    await consumer.Start()
 
 
 # Kafka Consumer 종료
 @app.on_event("shutdown")
-async def shutdown_event():
-    await consumer.stop()
+async def ShutdownEvent():
+    await consumer.Stop()
 
 
 # Tag 기반으로 가장 유사도가 높은 유저 검색
 @app.get("/api/matching-service/search-user")
-async def search_user(tags: str, topK: int = 5, topKperTag: int = 5):
+async def SearchUser(tags: str, topK: int = 5, topKperTag: int = 5):
     try:
         tags = tags.split(",")
         result = await asyncio.to_thread(SearchUsers, tags, topK, topKperTag)
@@ -35,10 +43,10 @@ async def search_user(tags: str, topK: int = 5, topKperTag: int = 5):
 
 
 # 유저의 대표 태그 검색 API
-@app.get("api/matching-service/represent-tags")
+@app.get("/api/matching-service/represent-tags")
 async def SearchRepresent(userID: int, topK: int):
     try:
-        tags = await asyncio.to_thread(RepresentTags, userID, topK)
+        tags = await RepresentTags(userID, topK)
         return {"status": 200, "message": "대표 태그 추출 성공", "data": tags}
     except Exception as e:
         return {
@@ -50,5 +58,5 @@ async def SearchRepresent(userID: int, topK: int):
 
 # 헬스 체크
 @app.get("/api/matching-service/health-check")
-async def health_check():
+async def HealthCheck():
     return {"status": 200, "message": "서버 상태 확인", "data": "Working"}
